f4c3a2daca2f325453195d65d688f1e51362bead
Daniil <115003840+11AgReS1SoR11@users.noreply.github.com>
2023-11-25 18:06:22 +0300

    Cat Fox Dog

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ import tkinter as tk
 from tkinter import messagebox
 import requests
 
-def handle_fox_search(endpoint_number):
-    try:
-        image_url = 'https://github.com/xinitrc-dev/randomfox.ca/blob/master/images/{}.jpg'.format(endpoint_number)
-        # Заменяем URL для просмотра сырого файла на GitHub
-        raw_url = image_url.replace('github.com', 'raw.githubusercontent.com').replace('/blob/', '/')
-        response = requests.get(raw_url)
-        if response.status_code == 200:
-            image = Image.open(BytesIO(response.content))
-            image.show()
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred: {e}")
+def get_image(url):
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    image.show()
 
-def handle_random_fox_search():
+def handle_random_search(url):
     try:
-        url = 'https://randomfox.ca/floof/'
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            response2 = requests.get(data["image"])
-            image = Image.open(BytesIO(response2.content))
-            image.show()
+            if "image" in data:
+                get_image(data["image"])
+            elif "url" in data:
+                # ignore gif and mp4
+                if (data["url"][-3:] == "mp4" or data["url"][-3:] == "gif" or data["url"][-3:] == "GIF"):
+                    handle_random_search(url)
+                    return
+                get_image(data["url"])
+            else:
+               raise ValueError("Broke url")
+
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
 
@@ -42,9 +42,9 @@ def main():
 
     root.configure(bg=bg_color)
 
-    button1 = tk.Button(root, text="FOX 1", command=lambda: handle_fox_search(1), bg=button_bg_color, height=5, width=40)
-    button2 = tk.Button(root, text="FOX 2", command=lambda: handle_fox_search(2), bg=button_bg_color, height=5, width=40)
-    button3 = tk.Button(root, text="FOX Random", command=lambda: handle_random_fox_search(), bg=button_bg_color, height=5, width=40)
+    button1 = tk.Button(root, text="CAT", command=lambda: get_image('https://cataas.com/cat'), bg=button_bg_color, height=5, width=40)
+    button2 = tk.Button(root, text="DOG", command=lambda: handle_random_search('https://random.dog/woof.json'), bg=button_bg_color, height=5, width=40)
+    button3 = tk.Button(root, text="FOX", command=lambda: handle_random_search('https://randomfox.ca/floof/'), bg=button_bg_color, height=5, width=40)
     exit_button = tk.Button(root, text="Exit", command=exit_application, bg="red", fg="white", height=5, width=40)
 
     button1.pack(pady=10)
